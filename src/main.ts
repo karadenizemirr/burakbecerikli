089b7b92d6439fe59414f1dfe5cf608f82117b3f
karadenizemirr <karadenizemirr@gmail.com>
2023-08-09 22:54:09 +0300

    First

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'src/static/assets'),
+    prefix: '/assets/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'src/static/views'),
+    layout: 'layout/main'
+  });
+  await app.listen(3000);
+}
+bootstrap();
